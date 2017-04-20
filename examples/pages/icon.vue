1ec6127dab1c6236f5cd17984d86032b9c99c7ad
Lan Qingyong <lanqy1314@gmail.com>
2017-04-20 14:14:07 +0800

    Add files via upload

--- examples/pages/icon.vue
+++ examples/pages/icon.vue
@@ -1,31 +1,37 @@
 <template>
   <div class="container docs-content">
-    <h1>Icon</h1>
+    <header class="text-center">
+      <h3>Icons</h3>
+    </header>
 
     <header class="text-center">
       <h4>Navigation</h4>
     </header>
-    
+
     <div class="columns">
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="arrow" dir="up"></s-icon>
         </s-button>
+        <p>icon-arrow-up</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="arrow" dir="right"></s-icon>
         </s-button>
+        <p>icon-arrow-right</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="arrow" dir="down"></s-icon>
         </s-button>
+        <p>icon-arrow-down</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="arrow" dir="left"></s-icon>
         </s-button>
+        <p>icon-arrow-left</p>
       </div>
     </div>
 
@@ -34,46 +40,55 @@
         <s-button type="primary" action size="lg">
           <s-icon icon="upward"></s-icon>
         </s-button>
+        <p>icon-upward</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="forward"></s-icon>
         </s-button>
+        <p>icon-forward</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="downward"></s-icon>
         </s-button>
+        <p>icon-downward</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="back"></s-icon>
         </s-button>
+        <p>icon-back</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="caret"></s-icon>
         </s-button>
+        <p>icon-caret</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="menu"></s-icon>
         </s-button>
+        <p>icon-menu</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="apps"></s-icon>
         </s-button>
+        <p>icon-apps</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="more" dir="vert"></s-icon>
         </s-button>
+        <p>icon-more-vert</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="more" dir="horiz"></s-icon>
         </s-button>
+        <p>icon-more-horiz</p>
       </div>
     </div>
 
@@ -86,46 +101,55 @@
         <s-button type="primary" action size="lg">
           <s-icon icon="resize" dir="horiz"></s-icon>
         </s-button>
+        <p>icon-resize-horiz</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="resize" dir="vert"></s-icon>
         </s-button>
+        <p>icon-resize-vert</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="plus"></s-icon>
         </s-button>
+        <p>icon-plus</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="minus"></s-icon>
         </s-button>
+        <p>icon-minus</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="cross"></s-icon>
         </s-button>
+        <p>icon-cross</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="check"></s-icon>
         </s-button>
+        <p>icon-check</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="stop"></s-icon>
         </s-button>
+        <p>icon-stop</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="shutdown"></s-icon>
         </s-button>
+        <p>icon-shutdown</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="refresh"></s-icon>
         </s-button>
+        <p>icon-refresh</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
@@ -136,36 +160,43 @@
         <s-button type="primary" action size="lg">
           <s-icon icon="flag"></s-icon>
         </s-button>
+        <p>icon-flag</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="bookmark"></s-icon>
         </s-button>
+        <p>icon-bookmark</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="edit"></s-icon>
         </s-button>
+        <p>icon-edit</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="delete"></s-icon>
         </s-button>
+        <p>icon-delete</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="share"></s-icon>
         </s-button>
+        <p>icon-share</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="download"></s-icon>
         </s-button>
+        <p>icon-download</p>
       </div>
       <div class="column col-3 col-md-6">
         <s-button type="primary" action size="lg">
           <s-icon icon="upload"></s-icon>
         </s-button>
+        <p>icon-upload</p>
       </div>
     </div>
 
