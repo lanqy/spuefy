083d4c4af564a286a75591e5d0b4be07c135f36e
Lan Qingyong <lanqy1314@gmail.com>
2017-04-20 14:38:34 +0800

    Add files via upload

--- examples/pages/button.vue
+++ examples/pages/button.vue
@@ -7,6 +7,11 @@
     <section class="notes">
       <p><strong>Buttons</strong> include simple button styles for actions in different types and sizes. </p>
     </section>
+    
+    <section class="notes">
+      <p>Add the <code>btn</code> class to &lt;a&gt;, &lt;input&gt; or &lt;button&gt; elements for a default button. There are classes <code>btn-primary</code> and <code>btn-link</code> for predefined primary and link buttons. A button with the <code>loading</code> class can show loading indicator.  </p>
+    </section>
+
       <div class="columns">
         <div class="column col-xs-12">
           <s-button @click="handleClick">Default button</s-button>
@@ -29,6 +34,10 @@
         <s-button type="link" loading>Link button</s-button>
       </div>
       </div>
+
+      <section class="notes">
+        <p>Add the <code>disabled</code> class or the <code>disabled</code> attribute for a disabled button.</p>
+      </section>
 
       <div class="columns">
         <div class="column col-xs-12">
@@ -86,7 +95,10 @@
             </s-button>
         </div>
       </div>
-
+      <section class="notes">
+        <p>Add the <code>btn-sm</code> or <code>btn-lg</code> class for small or large button size. Also, you can add the <code>btn-block</code> class for a full-width button. </p>
+        <p>Please note that you could use the <code>btn-action</code> class for a square button, or add another <code>circle</code> class for a round button, which is often used as a Float Action Button (FAB).</p>
+      </section>
 
       <header class="text-center">
         <h4></h4>
@@ -102,7 +114,8 @@
               <s-button type="primary" size="lg">
                 large
               </s-button>
-
+        </div>
+        <div class="column col-xs-12">
               <s-button type="primary" size="normal">
                 <s-icon icon="arrow" dir="left"></s-icon>
                 normal
@@ -111,7 +124,8 @@
               <s-button type="primary" size="normal">
                 normal
               </s-button>
-
+        </div>
+        <div class="column col-xs-12">
               <s-button type="primary" size="sm">
                 <s-icon icon="arrow" dir="left"></s-icon>
                 small
@@ -122,6 +136,9 @@
               </s-button>
         </div>
       </div>
+      <section class="notes">
+        <p>Icons with the <code>icon</code> class can be correctly rendered in each button size. </p>
+      </section>
   </div>
 </template>
 
