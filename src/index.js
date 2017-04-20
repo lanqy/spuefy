e110ad03830e7f6c167f17c28f89d3be922dff56
Lan Qingyong <lanqy1314@gmail.com>
2017-04-20 18:02:28 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import Link from './components/link'
 import ListItem from './components/list-item'
 import List from './components/list'
 import Icon from './components/icon'
+import Field from './components/field'
+import Input from './components/input'
+import Radio from './components/radio'
 
 const components = {
   Button,
   Link,
   ListItem,
   List,
-  Icon
+  Icon,
+  Field,
+  Input,
+  Radio
 }
 
 components.install = (Vue, options = {}) => {
