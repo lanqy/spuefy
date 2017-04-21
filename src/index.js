226cff6e8f4c4d3b6feb72b925f3a0bff316c977
Lan Qingyong <lanqy1314@gmail.com>
2017-04-21 17:30:31 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import ButtonGroup from './components/button-group'
 import Button from './components/button'
 import Link from './components/link'
 import ListItem from './components/list-item'
@@ -6,8 +7,13 @@ import Icon from './components/icon'
 import Field from './components/field'
 import Input from './components/input'
 import Radio from './components/radio'
+import RadioGroup from './components/radio-group'
+import Switch from './components/switch'
+import Checkbox from './components/checkbox'
+import CheckboxGroup from './components/checkbox-group'
 
 const components = {
+  ButtonGroup,
   Button,
   Link,
   ListItem,
@@ -15,7 +21,11 @@ const components = {
   Icon,
   Field,
   Input,
-  Radio
+  Radio,
+  RadioGroup,
+  Switch,
+  Checkbox,
+  CheckboxGroup
 }
 
 components.install = (Vue, options = {}) => {
